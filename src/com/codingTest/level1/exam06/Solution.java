0b4f661c34e4bbcb44f88bbec77b346e7b38d775
geonyeop123 <64470512+geonyeop123@users.noreply.github.com>
2024-07-12 19:25:11 +0900

    체육복

---
+++ src/com/codingTest/level1/exam06/Solution.java
@@ -0,0 +1,16 @@
+package com.codingTest.level1.exam06;
+
+public class Solution {
+
+
+    public int solution(String[][] board, int h, int w) {
+        int answer = 0;
+        return answer;
+    }
+
+
+    public static void main(String[] args) {
+
+
+    }
+}
