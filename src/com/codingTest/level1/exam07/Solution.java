8d04d94d1890768e9458b5b97360fe6f04accfb4
geonyeop123 <64470512+geonyeop123@users.noreply.github.com>
2024-10-28 22:24:44 +0900

    지폐접기

---
+++ src/com/codingTest/level1/exam07/Solution.java
@@ -0,0 +1,27 @@
+package com.codingTest.level1.exam07;
+
+public class Solution {
+
+    public int solution(int[] wallet, int[] bill) {
+        int answer = 0;
+        while(Math.min(wallet[0], wallet[1]) < Math.min(bill[0], bill[1]) ||
+                Math.max(wallet[0], wallet[1]) < Math.max(bill[0], bill[1])){
+
+            if(bill[0] > bill[1]){
+                bill[0] = bill[0] / 2;
+            }else{
+                bill[1] = bill[1] / 2;
+            }
+
+            answer++;
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println("hi");
+        System.out.println(solution.solution(new int[]{50,50}, new int[]{100, 241}));
+    }
+}
