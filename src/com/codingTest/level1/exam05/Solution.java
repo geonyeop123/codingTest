b79adf05ab1d645440be1bb5f3f6e5139087a070
geonyeop123 <64470512+geonyeop123@users.noreply.github.com>
2024-10-28 22:24:14 +0900

    카카오 아이디 만들기

--- src/com/codingTest/level1/exam05/Solution.java
+++ src/com/codingTest/level1/exam05/Solution.java
@@ -2,18 +2,59 @@ package com.codingTest.level1.exam05;
 
 /*
 
-햄버거 가게에서 일을 하는 상수는 햄버거를 포장하는 일을 합니다. 함께 일을 하는 다른 직원들이 햄버거에 들어갈 재료를 조리해 주면 조리된 순서대로 상수의 앞에 아래서부터 위로 쌓이게 되고, 상수는 순서에 맞게 쌓여서 완성된 햄버거를 따로 옮겨 포장을 하게 됩니다. 상수가 일하는 가게는 정해진 순서(아래서부터, 빵 – 야채 – 고기 - 빵)로 쌓인 햄버거만 포장을 합니다. 상수는 손이 굉장히 빠르기 때문에 상수가 포장하는 동안 속 재료가 추가적으로 들어오는 일은 없으며, 재료의 높이는 무시하여 재료가 높이 쌓여서 일이 힘들어지는 경우는 없습니다.
-예를 들어, 상수의 앞에 쌓이는 재료의 순서가 [야채, 빵, 빵, 야채, 고기, 빵, 야채, 고기, 빵]일 때, 상수는 여섯 번째 재료가 쌓였을 때, 세 번째 재료부터 여섯 번째 재료를 이용하여 햄버거를 포장하고, 아홉 번째 재료가 쌓였을 때, 두 번째 재료와 일곱 번째 재료부터 아홉 번째 재료를 이용하여 햄버거를 포장합니다. 즉, 2개의 햄버거를 포장하게 됩니다.
-상수에게 전해지는 재료의 정보를 나타내는 정수 배열 ingredient가 주어졌을 때, 상수가 포장하는 햄버거의 개수를 return 하도록 solution 함수를 완성하시오.
+카카오에 입사한 신입 개발자 네오는 "카카오계정개발팀"에 배치되어, 카카오 서비스에 가입하는 유저들의 아이디를 생성하는 업무를 담당하게 되었습니다. "네오"에게 주어진 첫 업무는 새로 가입하는 유저들이 카카오 아이디 규칙에 맞지 않는 아이디를 입력했을 때, 입력된 아이디와 유사하면서 규칙에 맞는 아이디를 추천해주는 프로그램을 개발하는 것입니다.
+다음은 카카오 아이디의 규칙입니다.
 
 */
 
 
 public class Solution {
 
+    StringBuffer sb = new StringBuffer();
+
+    public String solution(String new_id) {
+        // new_id의 모든 대문자를 대응되는 소문자로 치환합니다.
+        new_id = new_id.toLowerCase();
+
+        // new_id에서 알파벳 소문자, 숫자, 빼기, 밑줄, 마침표를 제외한 모든 문자를 제거합니다.
+        new_id = new_id.replaceAll("[^a-zA-Z-_.0-9]", "");
+
+        // 마침표(.)가 2번 이상 연속된 부분을 하나의 마침표로 치환합니다.
+        new_id = new_id.replaceAll("\\.+", ".");
+
+        // 마침표가 처음이나 끝에 위치한다면 제거합니다.
+        StringBuilder sb = new StringBuilder(new_id);
+        if(sb.charAt(0) == '.'){
+            sb.deleteCharAt(0);
+        }
+
+        if(!sb.isEmpty() && sb.charAt(sb.length()-1) == '.'){
+            sb.deleteCharAt(sb.length()-1);
+        }
+
+        // new_id가 빈 문자열이라면, new_id에 "a"를 대입합니다.
+        int length = sb.length();
+        if(length == 0){
+            sb.append("a");
+        }else if(length >= 16){
+            sb.delete(15, sb.length());
+        }
+
+        if(sb.charAt(sb.length()-1) == '.'){
+            sb.deleteCharAt(sb.length()-1);
+        }
+
+        while(sb.length() <= 2){
+            sb.append(sb.charAt(sb.length()-1));
+        }
+
+        return sb.toString();
+    }
 
     public static void main(String[] args) {
+        Solution s = new Solution();
 
+        System.out.println(s.solution("=.="));
     }
 
 }
