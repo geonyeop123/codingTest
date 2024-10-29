4d41e984ee2cdc5eddcc067763106065c8cb91c8
geonyeop123 <64470512+geonyeop123@users.noreply.github.com>
2024-10-29 14:55:41 +0900

    올바른 괄호

---
+++ src/com/codingTest/level2/exam01/Solution.java
@@ -0,0 +1,41 @@
+package com.codingTest.level2.exam01;
+
+public class Solution {
+
+    boolean solution(String s) {
+        int count = 0;
+
+        for(String ss : s.split("")){
+            if(ss.equals("(")){
+                count++;
+            }else{
+                count--;
+            }
+
+            if(count < 0){
+                return false;
+            }
+        }
+
+        return count == 0;
+    }
+
+    /* Stack으로 풀이 */
+//    boolean solution(String s) {
+//        Stack<String> stack = new Stack<>();
+//        for(String ss : s.split("")){
+//            if(ss.equals("(")){
+//                stack.push(ss);
+//            }else if(stack.empty()){
+//                return false;
+//            }else{
+//                stack.pop();
+//            }
+//        }
+//        return stack.isEmpty();
+//    }
+
+    public static void main(String[] args) {
+
+    }
+}
