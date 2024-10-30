d43f78ffddf2794da344c0d5ca4878ad385541d2
geonyeop123 <64470512+geonyeop123@users.noreply.github.com>
2024-10-30 11:53:11 +0900

    이진 변환 반복하기

---
+++ src/com/codingTest/level2/exam04/Solution.java
@@ -0,0 +1,32 @@
+package com.codingTest.level2.exam04;
+
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+/*
+* 이진 변환 반복하기
+*
+* */
+
+public class Solution {
+
+    public int[] solution(String s) {
+        int transferCount = 0;
+        int removeZeroCount = 0;
+
+        while(s.length() != 1){
+            int length = s.length();
+            s = s.replaceAll("0", "");
+            removeZeroCount += length - s.length();
+            transferCount++;
+            s = Integer.toBinaryString(s.length());
+        }
+
+        return new int[]{transferCount, removeZeroCount};
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+
+    }
+}
