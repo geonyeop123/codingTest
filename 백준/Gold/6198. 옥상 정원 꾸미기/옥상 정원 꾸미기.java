eb5b150ea4dba03e7d63da449ac7938df2f49c96
YEOP <64470512+geonyeop123@users.noreply.github.com>
2025-09-01 22:01:08 +0900

    [Gold V] Title: 옥상 정원 꾸미기, Time: 256 ms, Memory: 24132 KB -BaekjoonHub

---
+++ 백준/Gold/6198. 옥상 정원 꾸미기/옥상 정원 꾸미기.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.Stack;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int n = Integer.parseInt(br.readLine());
+        long answer = 0;
+        Stack<Integer> stack = new Stack<>();
+        for(int i = 0; i < n; i++){
+            int height = Integer.parseInt(br.readLine());
+            while(!stack.isEmpty()){
+                if(stack.peek() <= height){
+                    stack.pop();
+                }else{
+                    break;
+                }
+            }
+            answer += stack.size();
+            stack.push(height);
+        }
+        System.out.println(answer);
+    }
+}
